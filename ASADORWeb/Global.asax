6f7396b66bbfab63309a339fe9b08293d306042f
AsadorLP <asadorlaspalmeras@gmail.com>
2016-01-12 14:46:08 +0100

    Pagina web previa

---
+++ ASADORWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASADORWeb.MvcApplication" Language="C#" %>
---
+++ ASADORWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASADORWeb.Startup))]
+namespace ASADORWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
